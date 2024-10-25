6b7ab65dae2bb4ffcf58e987cfba92821f222e7d
alisherodilov005 <93549542+alisherodilov2@users.noreply.github.com>
2024-10-25 14:06:25 +0500

    changes

---
+++ models/HomeProblems.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "home_problems".
+ *
+ * @property int $id
+ * @property int $home_id
+ * @property string|null $problem
+ * @property string|null $type
+ * @property string|null $equipment_id
+ * @property string|null $created_at
+ * @property string|null $updated_at
+ *
+ * @property UserHomes $home
+ */
+class HomeProblems extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'home_problems';
+    }
+
+    /**
+     * @return \yii\db\Connection the database connection used by this AR class.
+     */
+    public static function getDb()
+    {
+        return Yii::$app->get('db2');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['home_id'], 'required'],
+            [['home_id'], 'integer'],
+            [['problem'], 'string'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['type', 'equipment_id'], 'string', 'max' => 255],
+            [['home_id'], 'exist', 'skipOnError' => true, 'targetClass' => UserHomes::class, 'targetAttribute' => ['home_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'home_id' => 'Home ID',
+            'problem' => 'Problem',
+            'type' => 'Type',
+            'equipment_id' => 'Equipment ID',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * Gets query for [[Home]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getHome()
+    {
+        return $this->hasOne(UserHomes::class, ['id' => 'home_id']);
+    }
+}
---
+++ views/problems/index.php
@@ -0,0 +1,46 @@
+<?php
+
+use app\models\HomeProblems;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var app\models\ProblemsSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Home problems';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="home-problems-index">
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <div class="card">
+        <div class="card-body">
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'columns' => [
+                    ['class' => 'yii\grid\SerialColumn'],
+
+                    'id',
+                    'home_id',
+                    'problem:ntext',
+                    'type',
+                    'equipment_id',
+                    //'created_at',
+                    //'updated_at',
+                    [
+                        'class' => ActionColumn::className(),
+                        'urlCreator' => function ($action, HomeProblems $model, $key, $index, $column) {
+                            return Url::toRoute([$action, 'id' => $model->id]);
+                        }
+                    ],
+                ],
+            ]); ?>
+        </div>
+    </div>
+
+
+</div>
